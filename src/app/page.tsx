8c9d6aeaea417bbde9bfc08ea97b25caa9e7ec97
jheo9 <jheo9@myseneca.ca>
2024-07-23 01:42:57 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Image from 'next/image';
+// import style from '@/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div className="flex justify-center">
+        <h1>Portfolio</h1>
+      </div>
+    </div>
+  );
+}

0e15957155b5692ed2c407223a03568810daaebb
jheo9 <jheo9@myseneca.ca>
2024-08-23 00:22:31 -0400

    Update 2024-08-23

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+
+
 export default function Header() {
     return (
       <header className="text-gray-600 body-font">
@@ -6,13 +9,17 @@ export default function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl">Header</span>
+            <span className="ml-3 text-xl">Brian</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-end">
-            <a className="mr-5 hover:text-gray-900">About me</a>
-            <a className="mr-5 hover:text-gray-900">Second Link</a>
-            <a className="mr-5 hover:text-gray-900">Third Link</a>
-            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+            <Link href = "/" className="mr-5 hover:text-gray-900">Home
+            </Link>
+           
+            <Link href = "/about-me" className="mr-5 hover:text-gray-900">About me
+            </Link>
+            <Link href="/project1" className="mr-5 hover:text-gray-900">Project</Link>
+          <Link href = "https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" className="mr-5 hover:text-gray-900">Contact Information
+          </Link>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
